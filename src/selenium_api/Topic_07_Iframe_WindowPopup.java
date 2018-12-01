2c7994cb59d91517f3bb1058bc328384acade654
ddon <ddon@DDON.SC.ESILICON.com>
2018-12-01 09:49:11 +0700

    up

--- src/selenium_api/Topic_07_Iframe_WindowPopup.java
+++ src/selenium_api/Topic_07_Iframe_WindowPopup.java
@@ -1,6 +1,8 @@
 package selenium_api;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
@@ -8,9 +10,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Topic_07_Iframe_WindowPopup {
-
+	
 	WebDriver driver;
-
+	Common c1 = new Common();
 	@BeforeClass
 	public void beforeClass() {
 		
@@ -27,7 +29,14 @@ public class Topic_07_Iframe_WindowPopup {
 	public void f() {
 //		Test Script 01:
 //			Step 01 - Truy cập vào trang: http://www.hdfcbank.com/
+		c1.open_page(driver, "http://www.hdfcbank.com/");
 //			Step 02 - Close popup nếu có hiển thị (switch qua iframe nếu có)  - F5 (refresh page) nhiều lần thì sẽ xuất hiện popup
+		By IframeWhatAreULooking4By = By.xpath("//div[@class='flipBannerWrap']//iframe");
+		WebElement IframeWhatAreULooking4 = driver.findElement(IframeWhatAreULooking4By);
+		By IframePopupCloseBy = By.xpath("//iframe[@id='vizury-notification-template']");
+		WebElement IframePopupClose = driver.findElement(IframePopupCloseBy);
+		By PopupCloseBy = By.xpath("//div[@id='div-close']");
+		WebElement PopupClose = driver.findElement(PopupCloseBy);
 //			Xpath iframe: //iframe[@id='vizury-notification-template']
 //
 //			Xpath close popup: //div[@id='div-close']
