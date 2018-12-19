dd04d9d339e7059014f95f09653f259cded01ed2
ddon <ddon@DDON.SC.ESILICON.com>
2018-12-19 17:45:51 +0700

    up

--- src/selenium_api/Topic_09_UploadFiles.java
+++ src/selenium_api/Topic_09_UploadFiles.java
@@ -12,6 +12,7 @@ import org.testng.annotations.Test;
 public class Topic_09_UploadFiles {
 
 	WebDriver driver;
+	Common c1 = new Common();
 
 	@BeforeClass
 	public void beforeClass() {
@@ -23,16 +24,22 @@ public class Topic_09_UploadFiles {
 		
 //		Firefox
 //		driver = new FirefoxDriver();
-		driver.get("https://google.com/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 //
 	@Test
-	public void f() {
-		
-		String homePageTitle = driver.getTitle();
-		AssertJUnit.assertEquals(homePageTitle, "Google");
+	public void TC01_UploadBySendkey() {
+//		Test Script 01: Upload file by sendkeys (multiple upload)
+//		Step 01 - Truy cập vào trang: 
+//		http://blueimp.github.com/jQuery-File-Upload/
+		c1.open_page(driver, "http://blueimp.github.com/jQuery-File-Upload/");
+//		Step 02 - Sử dụng phương thức sendKeys để upload file nhiều file cùng lúc chạy cho 3 trình duyệt (IE/ Firefox/ Chrome)
+//		Upload 3 file:
+//		Image01.png
+//		Image02.png
+//		Image03.png
+//		Step 03 - Kiểm tra file đã được chọn thành công
+//		Step 04 - Click Start button để upload cho cả 3 file
+//		Step 05 - Sau khi upload thành công verify cả 3 file đã được upload		
 	}
 //
 	@AfterClass
