ce7fffdd51bb4454bdf810040b3e022ff0566440
ddon <ddon@DDON.SC.ESILICON.com>
2018-12-19 15:00:41 +0700

    up

---
+++ src/selenium_api/Topic_09_UploadFiles.java
@@ -0,0 +1,43 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_09_UploadFiles {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		
+		
+		//Chrome
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+//		Firefox
+//		driver = new FirefoxDriver();
+		driver.get("https://google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+//
+	@Test
+	public void f() {
+		
+		String homePageTitle = driver.getTitle();
+		AssertJUnit.assertEquals(homePageTitle, "Google");
+	}
+//
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
